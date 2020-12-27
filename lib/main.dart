54f89e990a183d22cef6872cc639afd9facf8912
Francois Ritaly <francois.ritaly@gmail.com>
2020-12-27 16:56:40 +0100

    Added fields to the "Add a nihonto" form

--- lib/main.dart
+++ lib/main.dart
@@ -88,32 +88,32 @@ class _RandomWordsState extends State<RandomWords> {
 
   void _pushSaved() {
     Navigator.of(context).push(
-        MaterialPageRoute<void>(
-          builder: (BuildContext context) {
-            final tiles = _saved.map(
-                  (WordPair pair) {
-                return ListTile(
-                  title: Text(
-                    pair.asPascalCase,
-                    style: _biggerFont,
-                  ),
-                );
-              },
-            );
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
 
-            final divided = ListTile.divideTiles(
-              context: context,
-              tiles: tiles,
-            ).toList();
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
 
-            return Scaffold(
-              appBar: AppBar(
-                title: Text('Saved Suggestions'),
-              ),
-              body: ListView(children: divided),
-            );
-          },
-        ),
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
     );
   }
 
@@ -143,7 +143,6 @@ class NihontoForm extends StatefulWidget {
 }
 
 class NihontoFormState extends State<NihontoForm> {
-
   // Create a global key that uniquely identifies the Form widget and allows validation of the form.
   //
   // Note: This is a `GlobalKey<FormState>`, not a GlobalKey<NihontoFormState>.
@@ -154,12 +153,33 @@ class NihontoFormState extends State<NihontoForm> {
     // Build a Form widget using the _formKey created above.
     return Form(
         key: _formKey,
-        child: Column(
-            children: <Widget>[
-              // Add TextFormFields and ElevatedButton here.
-            ]
-        )
-    );
+        child: Column(children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+                labelText: 'Enter your username'
+            ),
+            // The validator receives the text that the user has entered.
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Validate returns true if the form is valid, otherwise false.
+              if (_formKey.currentState.validate()) {
+                // If the form is valid, display a snackbar. In the real world,
+                // you'd often call a server or save the information in a database.
+
+                Scaffold
+                    .of(context)
+                    .showSnackBar(SnackBar(content: Text('Processing Data')));
+              }
+            },
+            child: Text('Submit'),
+          )
+        ]));
   }
 }
-
