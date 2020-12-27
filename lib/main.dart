b8a192e74d7affecf9f27974aede6bcccbcfc205
Francois Ritaly <francois.ritaly@gmail.com>
2020-12-27 16:54:25 +0100

    Added route to "Add a new nihonto"

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,10 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// ==================== //
+// === Random words === //
+// ==================== //
+
 class RandomWords extends StatefulWidget {
   @override
   _RandomWordsState createState() => _RandomWordsState();
@@ -75,6 +79,7 @@ class _RandomWordsState extends State<RandomWords> {
         title: Text('Nihonto Collection Manager'),
         actions: [
           IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+          IconButton(icon: Icon(Icons.add), onPressed: _pushAdd),
         ],
       ),
       body: _buildSuggestions(),
@@ -111,4 +116,50 @@ class _RandomWordsState extends State<RandomWords> {
         ),
     );
   }
+
+  void _pushAdd() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Add a new nihonto'),
+            ),
+            body: NihontoForm(),
+          );
+        },
+      ),
+    );
+  }
 }
+
+// ==================== //
+// === Nihonto form === //
+// ==================== //
+
+class NihontoForm extends StatefulWidget {
+  @override
+  NihontoFormState createState() => NihontoFormState();
+}
+
+class NihontoFormState extends State<NihontoForm> {
+
+  // Create a global key that uniquely identifies the Form widget and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`, not a GlobalKey<NihontoFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+        key: _formKey,
+        child: Column(
+            children: <Widget>[
+              // Add TextFormFields and ElevatedButton here.
+            ]
+        )
+    );
+  }
+}
+
