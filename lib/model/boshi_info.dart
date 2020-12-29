5abb951b271f15a1e479944f25ca24952a6c6101
Francois Ritaly <francois.ritaly@gmail.com>
2020-12-29 19:06:37 +0100

    Added class BoshiInfo

---
+++ lib/model/boshi_info.dart
@@ -0,0 +1,49 @@
+import 'dart:math';
+
+import 'package:nihonto_collection_manager/model/boshi.dart';
+
+class BoshiInfo {
+
+  final _set = Set<Boshi>();
+
+  BoshiInfo([ Iterable<Boshi> args ]) {
+    if (args != null) {
+      _set.addAll(args);
+    }
+  }
+
+  BoshiInfo add(Boshi boshi) {
+    assert (boshi != null);
+
+    var set = Set<Boshi>.from(_set);
+    set.add(boshi);
+
+    return BoshiInfo(set);
+  }
+
+  BoshiInfo remove(Boshi boshi) {
+    assert (boshi != null);
+
+    var set = Set<Boshi>.from(_set);
+    set.remove(boshi);
+
+    return BoshiInfo(set);
+  }
+
+  static BoshiInfo random() {
+    final random = Random();
+
+    return BoshiInfo(Boshi.values.where((element) => random.nextBool()).toList());
+  }
+
+  bool getValue(Boshi boshi) {
+    assert (boshi != null);
+
+    return _set.contains(boshi);
+  }
+
+  @override
+  String toString() {
+    return 'BoshiInfo[set: ${_set}]';
+  }
+}
