a735ebc4a97edd4bd805b601461adb5a2d733633
Francois Ritaly <francois.ritaly@gmail.com>
2020-12-30 22:53:18 +0100

    Added widget WeightWidget

---
+++ lib/widget/weight_widget.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nihonto_collection_manager/extensions.dart';
+import 'package:nihonto_collection_manager/model/weight.dart';
+import 'package:nihonto_collection_manager/model/weight_unit.dart';
+import 'package:nihonto_collection_manager/utils.dart';
+
+class WeightWidget extends StatefulWidget {
+
+  static final TextInputFormatter decimalNumber =
+      FilteringTextInputFormatter.allow(RegExp(r'[0-9.]'));
+
+  // Can be null
+  Weight _weight;
+
+  WeightWidget({Key key, Weight weight}) : super(key: key) {
+    _weight = weight;
+  }
+
+  @override
+  WeightWidgetState createState() => WeightWidgetState(_weight);
+}
+
+class WeightWidgetState extends State<WeightWidget> {
+  double _value;
+  WeightUnit _unit;
+
+  WeightWidgetState([ Weight weight ]) {
+    if (weight != null) {
+      this._value = weight.value;
+      this._unit = weight.unit;
+    } else {
+      // Default to 0 g
+      this._value = 0;
+      this._unit = WeightUnit.GRAM;
+    }
+  }
+
+  Weight getWeight() {
+    return Weight(_value, _unit);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+      TextFormField(
+        decoration: InputDecoration(labelText: 'Value'),
+        initialValue: "${_value}",
+        key: Key(_value.toString()),
+        keyboardType: TextInputType.numberWithOptions(decimal: true),
+        inputFormatters: <TextInputFormatter>[
+          WeightWidget.decimalNumber
+        ],
+        onChanged: (value) {
+          setState(() {
+            _value = double.parse(value);
+          });
+        },
+      ),
+      DropdownButtonFormField(
+          decoration: InputDecoration(labelText: 'Unit'),
+          value: _unit,
+          key: Key(_unit.name),
+          items: Utils.getDropDownMenuItems(WeightUnit.values),
+          onChanged: (value) {
+            setState(() {
+              _unit = value;
+            });
+          }),
+    ]).pad();
+  }
+}
