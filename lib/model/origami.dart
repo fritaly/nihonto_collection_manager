b55e200d7f0c6728b71dac4ead5c1522da7e50c4
Francois Ritaly <francois.ritaly@gmail.com>
2021-01-02 18:55:28 +0100

    Added property Origami#other

--- lib/model/origami.dart
+++ lib/model/origami.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:nihonto_collection_manager/model/organization.dart';
 
+@immutable
 class Origami {
 
   final Organization issuer;
@@ -8,11 +10,13 @@ class Origami {
 
   final DateTime issuedAt;
 
-  Origami({Organization this.issuer, String this.type, DateTime this.issuedAt});
+  final String other;
+
+  Origami({this.issuer, this.type, this.issuedAt, this.other});
 
   @override
   String toString() {
-    return 'Origami[issuer: $issuer, type: $type, issuedAt: ${issuedAt}]';
+    return 'Origami[issuer: $issuer, type: $type, issuedAt: ${issuedAt}, other: ${other}]';
   }
 
   @override
@@ -22,8 +26,30 @@ class Origami {
           runtimeType == other.runtimeType &&
           issuer == other.issuer &&
           type == other.type &&
-          issuedAt == other.issuedAt;
+          issuedAt == other.issuedAt &&
+          this.other == other.other;
 
   @override
-  int get hashCode => issuer.hashCode ^ type.hashCode ^ issuedAt.hashCode;
+  int get hashCode => issuer.hashCode ^ type.hashCode ^ issuedAt.hashCode ^ other.hashCode;
+
+  Origami copyWith({
+    Organization issuer,
+    String type,
+    DateTime issuedAt,
+    String other,
+  }) {
+    if ((issuer == null || identical(issuer, this.issuer)) &&
+        (type == null || identical(type, this.type)) &&
+        (issuedAt == null || identical(issuedAt, this.issuedAt)) &&
+        (other == null || identical(other, this.other))) {
+      return this;
+    }
+
+    return new Origami(
+      issuer: issuer ?? this.issuer,
+      type: type ?? this.type,
+      issuedAt: issuedAt ?? this.issuedAt,
+      other: other ?? this.other,
+    );
+  }
 }
