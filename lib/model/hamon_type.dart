ebefb81a6adfbe261d8b7e72c72bfc320d580c47
Francois Ritaly <francois.ritaly@gmail.com>
2021-01-03 16:08:56 +0100

    Converted the enum HamonType to built_value

--- lib/model/hamon_type.dart
+++ lib/model/hamon_type.dart
@@ -1,57 +1,72 @@
-import 'package:nihonto_collection_manager/enum.dart';
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/built_value.dart';
+import 'package:built_value/serializer.dart';
 import 'package:nihonto_collection_manager/utils.dart';
 
-class HamonType extends EnumWithLabel<HamonType> {
+part 'hamon_type.g.dart';
 
-  static const SUGUHA = const HamonType._new('SUGUHA', 'Suguha');
-  static const GUNOME = const HamonType._new('GUNOME', 'Gunome');
-  static const CHOJI = const HamonType._new('CHOJI', 'Choji');
-  static const MIDARE = const HamonType._new('MIDARE', 'Midare');
-  static const NOTARE = const HamonType._new('NOTARE', 'Notare');
-  static const HITATSURA = const HamonType._new('HITATSURA', 'Hitatsura');
-  static const SUDARE = const HamonType._new('SUDARE', 'Sudare');
-  static const TORANBA = const HamonType._new('TORANBA', 'Toranba');
-  static const JUZUBA = const HamonType._new('JUZUBA', 'Juzuba');
-  static const KIKUSUI = const HamonType._new('KIKUSUI', 'Kikusui');
-  static const MIMIGATA = const HamonType._new('MIMIGATA', 'Mimigata');
-  static const NOKOGIRIBA = const HamonType._new('NOKOGIRIBA', 'Nokogiriba');
-  static const FUJI = const HamonType._new('FUJI', 'Fuji');
-  static const DORAN = const HamonType._new('DORAN', 'Doran');
-  static const OTHER = const HamonType._new('OTHER', 'Other');
+class HamonType extends EnumClass {
 
-  static const values = [
-    SUGUHA,
-    GUNOME,
-    CHOJI,
-    MIDARE,
-    NOTARE,
-    HITATSURA,
-    SUDARE,
-    TORANBA,
-    MIMIGATA,
-    KIKUSUI,
-    JUZUBA,
-    NOKOGIRIBA,
-    FUJI,
-    DORAN,
-    OTHER
-  ];
+  static Serializer<HamonType> get serializer => _$hamonTypeSerializer;
 
-  const HamonType._new(String name, String label) : super(name, label);
+  static const HamonType SUGUHA = _$SUGUHA;
+  static const HamonType GUNOME = _$GUNOME;
+  static const HamonType CHOJI = _$CHOJI;
+  static const HamonType MIDARE = _$MIDARE;
+  static const HamonType NOTARE = _$NOTARE;
+  static const HamonType HITATSURA = _$HITATSURA;
+  static const HamonType SUDARE = _$SUDARE;
+  static const HamonType TORANBA = _$TORANBA;
+  static const HamonType MIMIGATA = _$MIMIGATA;
+  static const HamonType KIKUSUI = _$KIKUSUI;
+  static const HamonType JUZUBA = _$JUZUBA;
+  static const HamonType NOKOGIRIBA = _$NOKOGIRIBA;
+  static const HamonType FUJI = _$FUJI;
+  static const HamonType DORAN = _$DORAN;
+  static const HamonType OTHER = _$OTHER;
 
-  static HamonType valueOf(String name) {
-    assert (name != null);
+  const HamonType._(String name) : super(name);
 
-    var found = values.firstWhere((element) => element.name == name);
+  static BuiltSet<HamonType> get values => _$values;
+  static HamonType valueOf(String name) => _$valueOf(name);
 
-    if (found == null) {
-      throw Exception("No enum found with name '${name}'");
+  String get label {
+    switch(this) {
+      case SUGUHA:
+        return 'Suguha';
+      case GUNOME:
+        return 'Gunome';
+      case CHOJI:
+        return 'Choji';
+      case MIDARE:
+        return 'Midare';
+      case NOTARE:
+        return 'Notare';
+      case HITATSURA:
+        return 'Hitatsura';
+      case SUDARE:
+        return 'Sudare';
+      case TORANBA:
+        return 'Toranba';
+      case MIMIGATA:
+        return 'Mimigata';
+      case KIKUSUI:
+        return 'Kikusui';
+      case JUZUBA:
+        return 'Juzuba';
+      case NOKOGIRIBA:
+        return 'Nokogiriba';
+      case FUJI:
+        return 'Fuji';
+      case DORAN:
+        return 'Doran';
+      case OTHER:
+        return 'Other';
+
+      default:
+        throw Exception('Unsupported value: ${this}');
     }
-
-    return found;
   }
 
-  static HamonType random() {
-    return Utils.random(values);
-  }
+  static HamonType random() => Utils.random(values.toList());
 }
