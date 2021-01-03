93e25b861e38a5a9c6538538eb3e2dce75b651e2
Francois Ritaly <francois.ritaly@gmail.com>
2021-01-03 15:54:04 +0100

    Converted the enum Boshi to built_value

--- lib/model/boshi.dart
+++ lib/model/boshi.dart
@@ -1,65 +1,84 @@
-import 'package:nihonto_collection_manager/enum.dart';
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/built_value.dart';
+import 'package:built_value/serializer.dart';
 import 'package:nihonto_collection_manager/utils.dart';
 
-class Boshi extends EnumWithLabel<Boshi> {
+part 'boshi.g.dart';
 
-  static const OMARU = const Boshi._new('OMARU', 'Omaru');
-  static const KOMARU = const Boshi._new('KOMARU', 'Komaru');
-  static const TOGARI = const Boshi._new('TOGARI', 'Togari');
-  static const MIDARE_KOMI = const Boshi._new('MIDARE_KOMI', 'Midare komi');
-  static const YAKIZUME = const Boshi._new('YAKIZUME', 'Yakizume');
-  static const JIZO = const Boshi._new('JIZO', 'Jizo');
-  static const HAKIKAKE = const Boshi._new('HAKIKAKE', 'Hakikake');
-  static const KAEN = const Boshi._new('KAEN', 'Kaen');
-  static const KAERI_FUKASHI = const Boshi._new('KAERI_FUKASHI', 'Kaeri fukashi');
-  static const KAERI_BUKA = const Boshi._new('KAERI_BUKA', 'Kaeri buka');
-  static const KAERI_ASASHI = const Boshi._new('KAERI_ASASHI', 'Kaeri asashi');
-  static const KAERI_TSUYOSHI = const Boshi._new('KAERI_TSUYOSHI', 'Kaeri tsuyoshi');
-  static const CHOJI_MIDARE_KOMI = const Boshi._new('CHOJI_MIDARE_KOMI', 'Choji midare komi');
-  static const NIE_KOZURE = const Boshi._new('NIE_KOZURE', 'Nie kozure');
-  static const ICHIMONJI_KAERI = const Boshi._new('ICHIMONJI_KAERI', 'Ichimonji kaeri');
-  static const KOMARU_NIERU = const Boshi._new('KOMARU_NIERU', 'Komaru nieru');
-  static const NOTARE_KOMI = const Boshi._new('NOTARE_KOMI', 'Notare komi');
-  static const ICHI_MAI = const Boshi._new('ICHI_MAI', 'Ichi mai');
-  static const OTHER = const Boshi._new('OTHER', 'Other');
+class Boshi extends EnumClass {
 
-  static const values = [
-    OMARU,
-    KOMARU,
-    TOGARI,
-    MIDARE_KOMI,
-    YAKIZUME,
-    JIZO,
-    HAKIKAKE,
-    KAEN,
-    KAERI_ASASHI,
-    KAERI_BUKA,
-    KAERI_FUKASHI,
-    KAERI_TSUYOSHI,
-    CHOJI_MIDARE_KOMI,
-    NIE_KOZURE,
-    ICHIMONJI_KAERI,
-    KOMARU_NIERU,
-    NOTARE_KOMI,
-    ICHI_MAI,
-    OTHER
-  ];
+  static Serializer<Boshi> get serializer => _$boshiSerializer;
 
-  const Boshi._new(String name, String label) : super(name, label);
+  static const Boshi OMARU = _$OMARU;
+  static const Boshi KOMARU = _$KOMARU;
+  static const Boshi TOGARI = _$TOGARI;
+  static const Boshi MIDARE_KOMI = _$MIDARE_KOMI;
+  static const Boshi YAKIZUME = _$YAKIZUME;
+  static const Boshi JIZO = _$JIZO;
+  static const Boshi HAKIKAKE = _$HAKIKAKE;
+  static const Boshi KAEN = _$KAEN;
+  static const Boshi KAERI_FUKASHI = _$KAERI_FUKASHI;
+  static const Boshi KAERI_BUKA = _$KAERI_BUKA;
+  static const Boshi KAERI_ASASHI = _$KAERI_ASASHI;
+  static const Boshi KAERI_TSUYOSHI = _$KAERI_TSUYOSHI;
+  static const Boshi CHOJI_MIDARE_KOMI = _$CHOJI_MIDARE_KOMI;
+  static const Boshi NIE_KOZURE = _$NIE_KOZURE;
+  static const Boshi ICHIMONJI_KAERI = _$ICHIMONJI_KAERI;
+  static const Boshi KOMARU_NIERU = _$KOMARU_NIERU;
+  static const Boshi NOTARE_KOMI = _$NOTARE_KOMI;
+  static const Boshi ICHI_MAI = _$ICHI_MAI;
+  static const Boshi OTHER = _$OTHER;
 
-  static Boshi valueOf(String name) {
-    assert (name != null);
+  const Boshi._(String name) : super(name);
 
-    var found = values.firstWhere((element) => element.name == name);
+  static BuiltSet<Boshi> get values => _$values;
+  static Boshi valueOf(String name) => _$valueOf(name);
 
-    if (found == null) {
-      throw Exception("No enum found with name '${name}'");
+  String get label {
+    switch(this) {
+      case OMARU:
+        return 'Omaru';
+      case KOMARU:
+        return 'Komaru';
+      case TOGARI:
+        return 'Togari';
+      case MIDARE_KOMI:
+        return 'Midare komi';
+      case YAKIZUME:
+        return 'Yakizume';
+      case JIZO:
+        return 'Jizo';
+      case HAKIKAKE:
+        return 'Hakikake';
+      case KAEN:
+        return 'Kaen';
+      case KAERI_FUKASHI:
+        return 'Kaeri fukashi';
+      case KAERI_BUKA:
+        return 'Kaeri buka';
+      case KAERI_ASASHI:
+        return 'Kaeri asashi';
+      case KAERI_TSUYOSHI:
+        return 'Kaeri tsuyoshi';
+      case CHOJI_MIDARE_KOMI:
+        return 'Choji midare komi';
+      case NIE_KOZURE:
+        return 'Nie kozure';
+      case ICHIMONJI_KAERI:
+        return 'Ichimonji kaeri';
+      case KOMARU_NIERU:
+        return 'Komaru nieru';
+      case NOTARE_KOMI:
+        return 'Notare komi';
+      case ICHI_MAI:
+        return 'Ichi mai';
+      case OTHER:
+        return 'Other';
+
+      default:
+        throw Exception('Unsupported value: ${this}');
     }
-
-    return found;
   }
 
-  static Boshi random() {
-    return Utils.random(values);
-  }
+  static Boshi random() => Utils.random(values.toList());
 }
